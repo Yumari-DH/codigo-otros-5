eb6f5bc3efb7aea6c381aea6ad974fb6b8f6e8d7
Yumari DH <yumari.diaz.herrera@gmail.com>
2025-09-18 15:44:52 -0600

    se corrigieron los errores de código y se añadieron comentarios para mejor entendimiento del código.

--- Codigo5.java
+++ Codigo5.java
@@ -1,29 +1,39 @@
-public class Codigo5 {
+//Este programa determina si el número dado por el usuario es afortunado o desafortunado
+//Se considera como número afortunado si tiene en sus dígitos mayor cantidad de
+//número afortunados (3,7,8,9) que los que no son afortunados.
 
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
-	    String ni = s.nextLine();
-	    int c = ni;
-	    
-	    int afo = 0;
-	    int noAfo = 0;
+import java.util.Scanner;
+
+public class Codigo5 {
+	//necesita un metodo main para poder ejecutar el código
+	public static void main( String[] args ){
+
+	    //solicitar número al usuario
+		Scanner s = new Scanner(System.in); //faltaba el System.in e importarlo
+		System.out.print("Introduzca un número: "); //usar "" o '' pero no ambas
+		int ni = Integer.parseInt( s.nextLine() );//convertir ni a int
+		int c = ni; 
+		
+		int afo = 0;
+		int noAfo = 0;
 	    
 	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
+		  int digito = ni % 10; //se borró el tipo de dato, no es necesario ponerlo
 	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
 			afo++;
 	      } else {
 			noAfo++;
-		  
-		  ni /= 10;
-	    }
+	    }//if else
+
+		 ni /= 10;//reducir el num de digitos, se movió afuera de else
 
 	    if (afo > noAfo) {
-	      System.out.prinln("El " + c + " es un número afortunado.");
+	      System.out.println("El " + c + " es un número afortunado."); //faltaba la t en println
 	    } else {
 	      System.out.println("El " + c + " no es un número afortunado.");
-	    }
-	    
-	  }
+	    }//if else  
+	  }//while
+	  s.close(); //cerrar el scann
 	  
-	}
+	}//main
+}//class
